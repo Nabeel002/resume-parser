086dbd0c967d91b7e3af993562837d6fb49eee27
NabeelHouzeo <nabeel.siddiqui@houzeo.com>
2026-05-31 03:29:42 +0530

    Add value prop to Input component for better control

--- app/components/Input.tsx
+++ app/components/Input.tsx
@@ -3,6 +3,7 @@ type InputProps = {
   label?: string;
   placeholder?: string;
   type?: string;
+  value?: string;
   onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
 };
 
@@ -11,6 +12,7 @@ const Input = ({
   placeholder = "text",
   onChange,
   type = "text",
+  value
 }: InputProps) => {
   return (
     <>
@@ -21,6 +23,7 @@ const Input = ({
           type={type}
           placeholder={placeholder}
           onChange={onChange}
+          value={value}
           className="mt-2 w-full rounded-lg bg-zinc-800 border border-zinc-700 px-3 py-2 text-white placeholder:text-gray-500 focus:outline-none focus:ring-2 focus:ring-white focus:border-indigo-500 transition"
         /> 
     </>
